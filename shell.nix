ac24d317d074f21ebdd6de2a39e5309548efb917
phdcybersec <82591009+phdcybersec@users.noreply.github.com>
2024-02-26 14:34:06 +0100

    :heavy_plus_sign: Add getopt to the shell.

--- shell.nix
+++ shell.nix
@@ -1,2 +1,30 @@
-(builtins.getFlake "github:phdcybersec/eiffel?rev=2cc73dc9d0489f08cdf0ea972b26a6892212488c")
-  .devShells.aarch64-darwin.default
+let
+  eiffel-flake = builtins.getFlake "github:phdcybersec/eiffel/4da0efe0bd501089b3f3355e46946d98fa10741c";
+  system = builtins.currentSystem;
+  pkgs = import eiffel-flake.inputs.nixpkgs { 
+    inherit system;
+    config.allowUnfree = true;
+  };
+  eiffel = eiffel-flake.packages.${system}.eiffel-env;
+in
+with pkgs; mkShell {
+  buildInputs = [
+    eiffel
+    
+    getopt
+    bintools
+    coreutils
+  ];
+  shellHook = ''
+    export EIFFEL_PYTHON_PATH=${eiffel}/bin/python
+  '' + (if stdenv.isLinux then ''
+    export LD_LIBRARY_PATH=${ lib.strings.concatStringsSep ":" [
+        "${cudaPackages.cudatoolkit}/lib"
+        "${cudaPackages.cudatoolkit.lib}/lib"
+        "${cudaPackages.cudnn}/lib"
+        "${cudaPackages.cudatoolkit}/nvvm/libdevice/"
+      ] }
+    
+    export XLA_FLAGS=--xla_gpu_cuda_data_dir=${cudaPackages.cudatoolkit}
+  '' else "");
+}
