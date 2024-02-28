56b388671f6f4c90feb7516620af4a47cdc344cc
phdcybersec <82591009+phdcybersec@users.noreply.github.com>
2024-02-28 12:52:21 +0100

    :alembic: Update analyses for the paper (RQ 0--2).

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
-  eiffel-flake = builtins.getFlake "github:phdcybersec/eiffel/b3865bd6be76d684cd3b020b94d67578a2bf247d";
+  rev = "6c3a1e7c25780d21033ba20d29e4921fc34508db";
+  eiffel-flake = builtins.getFlake "github:phdcybersec/eiffel/${rev}";
   system = builtins.currentSystem;
   pkgs = import eiffel-flake.inputs.nixpkgs { 
     inherit system;
@@ -9,14 +10,19 @@ let
 in
 with pkgs; mkShell {
   buildInputs = [
+    # Eiffel and Python dependencies
     eiffel
     
+    # LaTeX for matplotlib pgf backend
+    texlive.combined.scheme-full
+
+    # Shell environment
     getopt
     bintools
     coreutils
   ];
   shellHook = ''
-    export EIFFEL_PYTHON_PATH=${eiffel}/bin/python
+    export VSCODE_PYTHON_PATH=${eiffel}/bin/python
   '' + (if stdenv.isLinux then ''
     export LD_LIBRARY_PATH=${ lib.strings.concatStringsSep ":" [
         "${cudaPackages.cudatoolkit}/lib"
@@ -26,5 +32,7 @@ with pkgs; mkShell {
       ] }
     
     export XLA_FLAGS=--xla_gpu_cuda_data_dir=${cudaPackages.cudatoolkit}
+  '' else "") + (if builtins.pathExists ../eiffel/eiffel then ''
+    export PYTHONPATH="$(realpath ../eiffel/eiffel):$PYTHONPATH";
   '' else "");
 }
